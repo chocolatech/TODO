d0140735c286feaf70b9b0c42a262dc3cd39a79a
ewelina <evelinagonda@gmail.com>
2017-02-08 14:34:32 +0100

    refactor naming in add-item component

--- app/addItem/add-item.component.js
+++ app/addItem/add-item.component.js
@@ -5,11 +5,11 @@ angular.module('addItem').component('addItem', {
     templateUrl: 'addItem/add-item.template.html',
     controller: [function () {
         var vm = this;
-        vm.todoList = [{ todoText: 'Clean House', done: false }];
+        vm.items;
 
-        vm.todoAdd = function () {
-            vm.todoList.push({ todoText: vm.todoInput, done: false });
-            vm.todoInput = "";
+        vm.addNewItem = function () {
+
+            vm.newItem = "";
         };
 
     }]
