6529940f64b5b6021e45479bc7308d91fabd2ddc
ewelina <evelinagonda@gmail.com>
2017-02-08 15:40:06 +0100

    useless commit on add item

--- app/addItem/add-item.component.js
+++ app/addItem/add-item.component.js
@@ -1,16 +1,31 @@
 angular.module('addItem').component('addItem', {
     bindings: {
-        data: '='
+        data: '=',
+        onSubmit: '&'
     },
     templateUrl: 'addItem/add-item.template.html',
     controller: [function () {
         var vm = this;
-        vm.items;
 
-        vm.addNewItem = function () {
-
-            vm.newItem = "";
+        vm.$onInit = function $onInit() {
+            vm.newItem = {};
+            resetTodo();
         };
+
+        vm.submitForm = function submitForm() {
+            vm.onSubmit({
+                $event: {
+                    todo: vm.newItem
+                }
+            });
+
+            resetTodo();
+        };
+
+        function resetTodo() {
+            vm.newItem = {};
+        }
+
 
     }]
 })
