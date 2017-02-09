bc07cc32321503366de0dcd25564c6e69372ae2f
ewelina <evelinagonda@gmail.com>
2017-02-09 12:39:32 +0100

    remove const from app.module.js

--- app/app.module.js
+++ app/app.module.js
@@ -1,6 +1,4 @@
 angular.module('app', ['core', 'listItem', 'listContainer', 'addItem'])
-    .constant('Constants', {
-        states: ['todo', 'done', 'inProgress']
-    }).run(function (ListItemsService) {
+    .run(function (ListItemsService) {
        ListItemsService.initListItems();
     });
