9e3a1faf13c420085a1f42690dc305c8d5580348
ewelina <evelinagonda@gmail.com>
2017-02-09 15:03:45 +0100

    add line through when checked

--- app/listItem/list-item.component.js
+++ app/listItem/list-item.component.js
@@ -2,5 +2,14 @@ angular.module('listItem').component('listItem', {
     bindings: {
         data: '='
     },
-    templateUrl: 'listItem/list-item.template.html'
+    templateUrl: 'listItem/list-item.template.html',
+        controller: ['ListItemsService', function (ListItemsService) {
+        var vm = this;
+        vm.inputs = false;
+
+        vm.setInput = function () {
+            vm.inputs = !vm.inputs;
+        }
+
+    }]
 })
