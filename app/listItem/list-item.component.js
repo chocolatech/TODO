2a057ea52eaa7bc3d7668eef0faa6e735527eec4
ewelina <evelinagonda@gmail.com>
2017-02-09 15:44:33 +0100

    remove unused service in list item

--- app/listItem/list-item.component.js
+++ app/listItem/list-item.component.js
@@ -3,13 +3,21 @@ angular.module('listItem').component('listItem', {
         data: '='
     },
     templateUrl: 'listItem/list-item.template.html',
-        controller: ['ListItemsService', function (ListItemsService) {
+        controller: [function () {
         var vm = this;
         vm.inputs = false;
 
         vm.setInput = function () {
             vm.inputs = !vm.inputs;
+            //vm.changeState();
         }
+
+        // vm.changeState = function(){
+        //    var data = ListItemsService.getListItems();
+        //     if(vm.inputs){
+        //         data[1].state = 'lol';
+        //     }
+        // };
 
     }]
 })
